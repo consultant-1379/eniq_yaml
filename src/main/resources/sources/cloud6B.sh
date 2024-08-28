ad486be3d1e41e477350826d396ab8af5b681154
consultant <consultant-1379@tutamail.com>
2024-08-28 12:04:39 +0800

    first commit

---
+++ src/main/resources/sources/cloud6B.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+unset OS_PROJECT_ID
+unset OS_PROJECT_NAME
+unset OS_USER_DOMAIN_NAME
+
+export OS_AUTH_URL=https://cloud6b.athtem.eei.ericsson.se:13000/v2.0
+export OS_TENANT_NAME="ENIQ_C6B16"
+export OS_USERNAME="ENIQ_C6B16_admin"
+export OS_PASSWORD="admin123"
+export OS_PROJECT_ID="39b5468f1ae64b47b701044e1ae759a5"
